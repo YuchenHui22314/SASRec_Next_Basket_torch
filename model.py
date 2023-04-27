381cd51f0791f62013e8daa493d4cd6b4898e860
YuchenHui22314 <yuchen22314@gmail.com>
2023-04-27 01:07:05 -0400

    ban the key padding mask

--- model.py
+++ model.py
@@ -127,7 +127,7 @@ class SASRec(torch.nn.Module):
             mha_outputs, _ = self.attention_layers[i](
                 Q_K_V, Q_K_V, Q_K_V, 
                 attn_mask=attention_mask,
-                key_padding_mask=timeline_mask_float # doesn't work, because
+                #key_padding_mask=timeline_mask_float # doesn't work, because
                 )
                 # need_weights=False) this arg do not work?
             seqs = Q_K_V + mha_outputs
